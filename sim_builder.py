42d1608589f323eee3a245bc8bb93c648494ea3f
Tyler Banks <tbg28@mail.missouri.edu>
2018-03-03 23:50:51 -0600

    gui updates

--- sim_builder.py
+++ sim_builder.py
@@ -8,19 +8,27 @@ Created on Sat Feb 24 23:26:54 2018
 import pandas as pd
 import re
 import glob
+from collections import defaultdict
 
 try:
     import Tkinter as tk # this is for python2
     import ttk
+    import tkMessageBox as messagebox
 except:
     import tkinter as tk # this is for python3
     from tkinter import ttk
+    from tkinter import messagebox
 
 dataset_folder = 'datasets'
 cells_folder = 'cells'
 
-cellnums_glob = dataset_folder+'/cellnumbers_*.dat'
-connections_glob = dataset_folder+'/conndata_*.dat'
+cellnums_file_prefix = 'cellnumbers_'
+cellnums_file_postfix = '.dat'
+conndata_file_prefix = 'conndata_'
+conndata_file_postfix = '.dat'
+
+cellnums_glob = dataset_folder+'/'+ cellnums_file_prefix + '*' + cellnums_file_postfix
+connections_glob = dataset_folder+'/' + conndata_file_prefix +'*'+conndata_file_postfix
 
 cells_glob = cells_folder+'/class_*.hoc'
 
@@ -80,8 +88,36 @@ class CreateToolTip(object):
             tw.destroy()
           
             
+class DialogEntryBox:
+    def __init__(self, parent, text="value"):
+
+        top = self.top = tk.Toplevel(parent)
+        top.geometry('200x100')
+        tk.Label(top, text=text).pack()
+        
+        self.value = tk.StringVar(top)
+        self.confirm = False
+        
+        self.e = tk.Entry(top,textvariable=self.value)
+        self.e.pack(padx=5)
+
+        button_frame = tk.Frame(top)
+        button_frame.pack()
+        
+        b = tk.Button(button_frame, text="Ok", command=self.ok)
+        b.grid(pady=5, padx=5, column=0, row=0)
+        
+        b = tk.Button(button_frame, text="Cancel", command=self.cancel)
+        b.grid(pady=5, padx=5, column=1, row=0)
+
+    def ok(self):
+        self.confirm = True
+        self.top.destroy()
+    def cancel(self):
+        self.top.destroy()
+        
 class PandasTable(tk.Frame):
-    '''Easily display an editable pandas dataframe in TK as a Frame (Banks)'''
+    '''Easily display an editable pandas dataframe in TK as a Frame (Created by Tyler Banks)'''
     '''
     root = None
     table_frame_internal = None
@@ -93,17 +129,22 @@ class PandasTable(tk.Frame):
     '''
     def __init__(self, widget):
         super(PandasTable, self).__init__(widget)
-        self.root = tk.Frame(widget)
-        self.table_frame_internal = tk.Frame(self.root)
+        self.root = tk.Frame(widget,padx=5,pady=5)
+        self.table_frame_internal = tk.Frame(self.root,background='white')
         self.table_tools_frame_internal = tk.Frame(self.root)
+        #self.table_frame_internal.grid_forget() #Probably good housekeeping
+        #self.table_frame_internal.destroy() #Re-enable for previously created
         self.table_frame_internal.grid(sticky="news",row=0,column=0)
         self.table_tools_frame_internal.grid(sticky="news",row=1,column=0)
+        self.options_dict = None
         self.init_tools()
+        self.data_changed = False
+        self.show_header = True
         return
     
     def init_tools(self):
         addRowButton = tk.Button(self.table_tools_frame_internal, text="Add Row", command=lambda: self.add_row(None))
-        addRowButton.grid(column=0, row =0, padx=5, sticky='W')
+        addRowButton.grid(column=0, row =0, padx=5,pady=5, sticky='W')
         
     def pack(self,*args):
         super(PandasTable,self).pack(*args)
@@ -113,29 +154,47 @@ class PandasTable(tk.Frame):
     def grid(self,*args):
         super(PandasTable,self).grid(*args)
         self.root.grid(*args)
-    
-    def set_dataframe(self, df):
+        
+    def set_changed(self, ch):
+        self.data_changed = ch
+    def change_in_data(self, *args):
+        self.set_changed(True)
+    def has_changed(self):
+        return self.data_changed
+       
+    def set_dataframe(self, df, options_dict = defaultdict(list), show_header=True):
         '''Totally wipe the slate and display a new dataframe'''
+        self.data_changed = False
+        self.show_header = show_header
         for widget in self.table_frame_internal.winfo_children():
             widget.destroy()
         self.entities_arr = []
         self.values_arr = []
         self.deleted_rows = []
+        self.options_dict = options_dict
         
-        self.names = list(df)        
-        for k, n in enumerate(self.names):
-            var = tk.Label(self.table_frame_internal, text=n)
-            var.config(width=15,relief=tk.GROOVE)
-            var.grid(column=k, row =0, padx=5, sticky='W')
+        self.names = list(df)
         
+        if show_header:
+            for k, n in enumerate(self.names):
+                var = tk.Label(self.table_frame_internal, text=n)
+                var.config(width=15,relief=tk.GROOVE,background='light gray')
+                var.grid(column=k, row =0, padx=5, sticky='NEWS')
+                
         for i, row in df.iterrows():
             self.add_row(row)            
     
         return
     
     def get_dataframe(self):
-        #for each row if not in deleted
-        return
+        l = []
+        for i, val_row in enumerate(self.values_arr):
+            r = []
+            if i not in self.deleted_rows:
+                for j, val in enumerate(val_row):
+                    r.append(val.get())
+                l.append(r)
+        return pd.DataFrame(l,columns=self.names)
     
     def add_row(self,row):
         col_arr = []
@@ -154,17 +213,17 @@ class PandasTable(tk.Frame):
         for j, col in enumerate(row):
             value = tk.StringVar(self.table_frame_internal)
             value.set(col)
+            value.trace("w",self.change_in_data)
             entity = None
-            if j == 10000:
-                entity = tk.OptionMenu(self.table_frame_internal, value, *cellclasses_a)
+            
+            if self.options_dict is not None and self.options_dict.get(j,False):
+                entity = tk.OptionMenu(self.table_frame_internal, value, *self.options_dict.get(j)[0])
                 entity.config(width=20)
-                entity.grid(column=j,row=insert_i,sticky='W')
+                entity.grid(column=j,row=insert_i,sticky='NEWS')
             else:
-                entity = tk.Entry(self.table_frame_internal)
-                entity.delete(0,tk.END)
-                entity.insert(0,value.get())
+                entity = tk.Entry(self.table_frame_internal,textvariable=value)
                 entity.place(width=20)
-                entity.grid(row=insert_i,column=j,sticky='E')
+                entity.grid(row=insert_i,column=j,sticky='NEWS')
                 
             entity_arr.append(entity)
             col_arr.append(value)
@@ -184,6 +243,7 @@ class PandasTable(tk.Frame):
             for j,c in enumerate(r):
                 if(i==row):
                     c.grid_forget()
+                    c.destroy()
 
         for i in self.deleted_rows:
             if i == row:
@@ -191,14 +251,19 @@ class PandasTable(tk.Frame):
         self.deleted_rows.append(row)
         return
     
-    def has_changed(self):
-        return True
-        
+    def new(self):
+        df = pd.DataFrame(columns=self.names)
+        self.set_dataframe(df, options_dict=self.options_dict, show_header=self.show_header)
+        self.add_row(None)
+        return
     
 def menu_bar(root):
     def hello():
         print("hello!")
         
+    def about():
+        messagebox.showinfo("About", "Written for Satish Nair's Computational Neuroscience Lab\n\nContributors:\nTyler Banks\nBen Latimer", icon='info')
+
     menubar = tk.Menu(root)
     
     # create a pulldown menu, and add it to the menu bar
@@ -210,7 +275,7 @@ def menu_bar(root):
     menubar.add_cascade(label="File", menu=filemenu)
     
     helpmenu = tk.Menu(menubar, tearoff=0)
-    helpmenu.add_command(label="About", command=hello)
+    helpmenu.add_command(label="About", command=about)
     menubar.add_cascade(label="Help", menu=helpmenu)
     return menubar
 
@@ -284,61 +349,97 @@ def parameters_page(frame):
 
 def cells_page(root):
     
-    top_option_frame = tk.Frame(root)
-    table_frame = tk.Frame(root)
+    top_option_frame = tk.LabelFrame(root, text="File Management")
+    table_frame = tk.LabelFrame(root, text="Cell Numbers")
     bottom_option_frame = tk.Frame(root)
     
-    top_option_frame.pack()
-    table_frame.pack()
-    bottom_option_frame.pack()
+    top_option_frame.grid(column=0,row=0,sticky='news',padx=10,pady=5)
+    table_frame.grid(column=0,row=1,sticky='news',padx=10,pady=5)
+    bottom_option_frame.grid(column=0,row=2)
     
+    pt = PandasTable(table_frame)
+    
+    cellclasses_a = []
+    options = glob.glob(cellnums_glob)
+    
+    def generate_files_available():
+        cellclasses = glob.glob(cells_glob)
+        cellclasses_a.clear()
+        search = 'cells\\\\class_(.+?).hoc'
+        for c in cellclasses:
+            m = re.search(search, c)
+            if m:
+                cellclasses_a.append(m.group(1))
     
     def load(*args):
-        print ("loading: " + filename.get())
+        #print ("loading: " + filename.get())
         cellnums_pd = pd.read_csv(filename.get() ,delimiter=' ',\
-                           skiprows=1,header=None,\
-                           names = ["Friendly Cell Name", "Cell File Name", "Num Cells", "Layer Index","Real:1 Artificial:0"])
+                       skiprows=1,header=None,\
+                       names = ["Friendly Cell Name", "Cell File Name", "Num Cells", "Layer Index","Real:1 Artifical:0"])
         
-        pt = PandasTable(table_frame)
-        pt.set_dataframe(cellnums_pd)
+        pt.set_dataframe(cellnums_pd, options_dict=d)
         pt.pack()
        
     def save():
+        pt_df = pt.get_dataframe()
+        (nr,nc) = pt_df.shape 
+        tb = pt_df.to_csv(sep=' ',header=False,index=False)
+        
+        file = open(filename.get(),"w")
+        file.write(str(nr)+'\n')
+        file.write(tb)
+        file.close()
         return
     
     def new():
-        return
+        if pt.has_changed():
+            result = messagebox.askquestion("New", "Are you sure? Data has been changed.", icon='warning')
+            if result != 'yes':
+                return
+        d = DialogEntryBox(root,text="New File Name:")
+        root.wait_window(d.top)
+        
+        if d.confirm==False:
+            return
+        
+        newfilename = dataset_folder+'\\'+cellnums_file_prefix+ d.value.get() + cellnums_file_postfix
+        f = open(newfilename,"w+")
+        f.close
+        #pt.new()
+        generate_files_available()
+        #https://stackoverflow.com/questions/17580218/changing-the-options-of-a-optionmenu-when-clicking-a-button
     
-    options = glob.glob(cellnums_glob)
-    cellclasses = glob.glob(cells_glob)
-    cellclasses_a = []
+        m = fileMenu.children['menu']
+        m.delete(0,tk.END)
+        newvalues = options
+        newvalues.append(newfilename)
+        for val in newvalues:
+            m.add_command(label=val,command=lambda v=filename,l=val:v.set(l))
+        filename.set(newfilename)
+        
+        pt.new()
+
+    generate_files_available()
     
-    search = 'cells\\\\class_(.+?).hoc'
-    for c in cellclasses:
-        m = re.search(search, c)
-        if m:
-            cellclasses_a.append(m.group(1))
-    
-    
-    #print(cellclasses)
-    
+    d = defaultdict(list)
+    d[1].append(cellclasses_a)
+        
     #Create the choice option panel
-    
     filename = tk.StringVar(top_option_frame)
     filename.trace("w",load)
     filename.set(options[0])
+    load()#initial load
     
     fileMenu = tk.OptionMenu(top_option_frame, filename, *options)
     fileMenu.grid(column=1, row =0, padx=5, sticky='W')
+    
+    
     saveButton = tk.Button(top_option_frame, text="Save", command=save)
     saveButton.grid(column=2, row =0, padx=5, sticky='W')
-    newButton = tk.Button(top_option_frame, text="New", command=save)
+    newButton = tk.Button(top_option_frame, text="New", command=new)
     newButton.grid(column=0, row =0, padx=5, sticky='W')
     useButton = tk.Button(top_option_frame, text="Set as NumData parameter", command=save)
     useButton.grid(column=3, row =0, padx=5, sticky='W')
-    
-    
-    #File grids
     
     
     
@@ -368,14 +469,16 @@ def connections_page(root):
     button = tk.Button(root, text="ok", command=ok)
     button.pack()
     
-    #print(read_connections())
+    pt = PandasTable(root)
+    pt.set_dataframe(read_connections())
+    pt.pack()
     
 
 def synapses_page(root):
     
-    cellnums_pd = pd.read_csv('datasets/cellnumbers_104.dat' ,delimiter=' ',\
+    cellnums_pd = pd.read_csv('datasets/syndata_126.dat' ,delimiter=' ',\
                            skiprows=1,header=None,\
-                           names = ["Friendly Cell Name", "Cell File Name", "Num Cells", "Layer Index","Real:1 Artificial:0"])
+                           names = ["Friendly Cell Name", "Cell File Name", "Num Cells", "Layer Index","Real:1 Artifical:0"])
     pt = PandasTable(root)
     pt.set_dataframe(cellnums_pd)
     pt.pack()
@@ -400,10 +503,10 @@ def main():
     page6 = ttk.Frame(nb)
     page7 = ttk.Frame(nb)
     
-    nb.add(page4, text='Synapses')
     nb.add(page1, text='Parameters')
     nb.add(page2, text='Cells')
     nb.add(page3, text='Connections')
+    nb.add(page4, text='Synapses')
     nb.add(page5, text='Phasic Data')
     nb.add(page6, text='Cell Builder')
     nb.add(page7, text='Simulation Builder')
